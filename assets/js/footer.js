aa1c5b4caa5bdd1557c485990cf031dd006029bd
diogofr4 <diogofr4@gmail.com>
2017-10-17 05:11:55 -0200

    Adicionando footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,5 +1,5 @@
 // When the user clicks on the button, scroll to the top of the document
-$('.returnar-ao-topo').click(function() {      // When arrow is clicked
+$('.retornar-ao-topo').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
