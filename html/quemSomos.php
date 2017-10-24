6a9097eab0c5d3f0ce9ba3660bb926cc9ec26e31
duduvalle <evalle@ice.ufjf.br>
2017-10-24 12:22:42 -0200

    Alterando a extensao de html para php

--- html/quemSomos.php
+++ html/quemSomos.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/quemSomos.css">
 </head>
 <body>
 	<section id="quem-somos" class="container">
