c0db5ad92cb627d1e814af531aebeb0c727ba574
diogofr4 <diogofr4@gmail.com>
2017-10-24 22:00:40 -0200

    Adicionado arquivos em php, deletado bootstrap

---
+++ html/footer.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Yoshi Store</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="../assets/img/logo/Yoshis_Store_-_Logo.ico" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/footer.css">
+
+</head>
+<body>
+  <!-- __________________________________________________________________________________________________________________________________ -->
+  <!-- Rodapé -->
+  <footer>
+    <div class="footer-principal">
+      <!-- Botão de retornar ao topo -->
+      <div class="retornar-ao-topo">
+        <button type="button" class="btn btn-default btn-lg">
+          <i class="fa fa-angle-up fa-2x"></i>
+        </button>
+      </div>
+      <!-- Fim botão de retornar ao topo -->
+      <div class="row no-margin">     
+        <div class="col-md-2 col-md-offset-1">
+          <div class="menu">
+            <h5>Navegação</h5>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Produtos</a></li>
+              <li><a href="#">Contato</a></li>
+              <li><a href="#">Sobre nós</a></li>
+              <li><a href="#">Localização</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-2 col-md-offset-6">
+          <div class="menu">
+            <h5>Yoshi's Store</h5>
+            <ul>
+              <p>Rua Yoshi, Bairro Island, Mario World.</p>
+              <p>(32)99999-9999</p>
+              <p>(32)3232-3232</p>
+              <p>yoshistore@yoshi.com</p>
+            </ul>
+          </div>
+        </div>
+      <div class="col-md-3 col-md-offset-9">
+        <div class="redes-sociais">
+          <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+          <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+          <a href="#" class="youtube"><i class="fa fa-youtube"></i></a>
+        </div>
+      </div>
+      </div>
+    </div>
+    <!-- Segunda parte do footer -->
+    <div class="footer-copyright">
+      <p>Yoshi's Store © 2017 - TODOS OS DIREITOS RESERVADOS </p>
+    </div>
+    <!-- Fim da segunda parte do footer -->
+  </footer>
+  <!-- Fim rodapé -->
+  <script src="../assets/js/jquery-1.9.1.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../assets/js/footer.js"></script>
+  <script src="https://use.fontawesome.com/6f8b7aea2d.js"></script>
+</body>
+</html>
