b077e40c8d69e859294fb71924eb72d09f7e3a52
diogofr4 <diogofr4@gmail.com>
2017-11-01 17:52:10 -0200

    Adicionado back-end da pagina inicial

--- projeto1.sql
+++ projeto1.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 22-Out-2017 às 20:58
--- Versão do servidor: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Host: 127.0.0.1:3306
+-- Generation Time: 01-Nov-2017 às 19:50
+-- Versão do servidor: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,10 +28,25 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `plataforma`
 --
 
-CREATE TABLE `plataforma` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `plataforma`;
+CREATE TABLE IF NOT EXISTS `plataforma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `plataforma`
+--
+
+INSERT INTO `plataforma` (`id`, `nome`) VALUES
+(1, 'PC'),
+(2, 'Wii U'),
+(3, 'GameBoy Color'),
+(4, 'PS3'),
+(5, 'Playstation 4'),
+(6, 'Mega Drive'),
+(7, 'Xbox One');
 
 -- --------------------------------------------------------
 
@@ -39,17 +54,36 @@ CREATE TABLE `plataforma` (
 -- Estrutura da tabela `produto`
 --
 
-CREATE TABLE `produto` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE IF NOT EXISTS `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
-  `preco` float NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
   `descricao` mediumtext,
   `data_lancamento` date DEFAULT NULL,
   `plataforma_id` int(11) DEFAULT NULL,
   `tipo_id` int(11) DEFAULT NULL,
   `quantidade_estoque` int(11) DEFAULT NULL,
-  `usado` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `usado` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plataforma_id` (`plataforma_id`),
+  KEY `tipo_id` (`tipo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `preco`, `descricao`, `data_lancamento`, `plataforma_id`, `tipo_id`, `quantidade_estoque`, `usado`) VALUES
+(1, 'The Witcher 3', '50.25', NULL, NULL, 1, 1, 1, 0),
+(2, 'Mega Drive', '99.99', NULL, NULL, 6, 3, 1, 0),
+(3, 'Super Luigi U', '85.99', NULL, NULL, 2, 1, 1, 1),
+(4, 'Pokemon Crystal Version', '49.99', NULL, NULL, 3, 1, 1, 0),
+(5, 'GTA 5', '99.90', NULL, NULL, 4, 1, 1, 0),
+(7, 'Playstation 4', '1299.99', NULL, NULL, 5, 3, 1, 0),
+(9, 'Overwatch', '150.99', NULL, NULL, 1, 1, 1, 0),
+(10, 'Destiny 2', '150.99', NULL, NULL, 1, 1, 1, 0),
+(11, 'Need for Speed Most Wanted', '99.99', NULL, NULL, 1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -57,10 +91,21 @@ CREATE TABLE `produto` (
 -- Estrutura da tabela `tipo`
 --
 
-CREATE TABLE `tipo` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `tipo`;
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipo`
+--
+
+INSERT INTO `tipo` (`id`, `nome`) VALUES
+(1, 'Jogo'),
+(2, ''),
+(3, 'Console');
 
 -- --------------------------------------------------------
 
@@ -68,69 +113,13 @@ CREATE TABLE `tipo` (
 -- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(20) NOT NULL,
-  `senha` varchar(45) NOT NULL
+  `senha` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `plataforma`
---
-ALTER TABLE `plataforma`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `produto`
---
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `plataforma_id` (`plataforma_id`),
-  ADD KEY `tipo_id` (`tipo_id`);
-
---
--- Indexes for table `tipo`
---
-ALTER TABLE `tipo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `plataforma`
---
-ALTER TABLE `plataforma`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `produto`
---
-ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tipo`
---
-ALTER TABLE `tipo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
