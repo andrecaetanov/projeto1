59d5eecbcdd60f2d24c32864c33180d72f8cb304
diogofr4 <diogofr4@gmail.com>
2017-11-10 00:49:20 -0200

    adicionado footer e header do admin

---
+++ admin/header.php
@@ -0,0 +1,23 @@
+<!-- COMEÇO DO MENU DE NAVEGAÇÃO -->
+<div class="container-header">
+  <div class="box">
+    <header>
+      <nav id='cssmenu'>
+        <div class="navbar-header">
+          <a class="navbar-brand nav-header"><img src="assets/img/Yoshis_Store_-_Logo.png"></a>
+          <div class="navbar-brand nav-header" id="logo">Yoshi's Store</div>
+        </div>
+        <div id="head-mobile"></div>
+        <div class="button"></div>
+        <ul class="conteudo">
+          <li><a href='index.php'>Home</a></li>
+          <li><a href='quemSomos.php'>Tipos</a></li>
+          <li><a href='#'>Plataformas</a>
+          <li><a href='#'>Produtos</a>
+          <li><a href='#'>Usuários</a>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</div>
+<!-- FIM DO MENU DE NAVEGAÇÃO-->
