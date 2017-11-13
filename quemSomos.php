994b19e8b3415c3e3f7fd898365680ab03f2686b
Thales Castro <thalescastro.m@gmail.com>
2017-11-13 18:53:59 -0200

    arrumando os links do quemSomos

--- quemSomos.php
+++ quemSomos.php
@@ -4,20 +4,20 @@
 	<title>Quem somos</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" type="text/css" href="../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/quemSomos.css">
-	<link rel="shortcut icon" href="../assets/img/logo/Yoshis_Store_-_Logo.ico" type="image/x-icon" />
-	<link rel="stylesheet" type="text/css" href="../assets/css/footer.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/quemSomos.css">
+	<link rel="shortcut icon" href="assets/img/logo/Yoshis_Store_-_Logo.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="assets/css/footer.css">
 	<link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
-	<link rel="stylesheet" type="text/css" href="../assets/css/header.css">
-	<link rel="icon" href="../assets/img/Yoshis_Store_-_Logo.png">
+	<link rel="stylesheet" type="text/css" href="assets/css/header.css">
+	<link rel="icon" href="assets/img/Yoshis_Store_-_Logo.png">
 </head>
 <body>
 <?php include ("header.php"); ?>
 <section id="quem-somos" class="container">
 	<!-- Inicio do texto institucional -->
 	<h1 id="titulo">Sobre Nós</h1>
-	<img class="img-responsive col-sm-6" src="../assets/img/Yoshis_Store_-_Logo.png" alt="Logo da empresa">
+	<img class="img-responsive col-sm-6" src="assets/img/Yoshis_Store_-_Logo.png" alt="Logo da empresa">
 	<div class="col-sm-6">
 		<h2>Missão</h2>
 		<p class="texto-institucional">Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla</p>
